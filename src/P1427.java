b6e65531459c85a413879e17f33a6dd2b1758d17
munzii <munzii422@gmail.com>
2021-12-14 22:45:10 +0900

    studying

---
+++ src/P1427.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class P1427 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String n = br.readLine();
+		char[] arr = n.toCharArray();
+		Arrays.sort(arr);
+		
+		for(int i=arr.length-1; i>=0; i--) {
+			bw.write(arr[i]);
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
